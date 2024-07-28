aff0323caf9fc7c7ee4152a3653c8c3ccbe232be
Patrick <juanpatrick.gallardo@gmail.com>
2024-07-28 16:58:03 +0800

    Created Introduction page

--- frontend/src/components/dashboard.tsx
+++ frontend/src/components/dashboard.tsx
@@ -1,10 +1,11 @@
 import Navbar from "./navbar";
+import About from "./about"
 
 const Dashboard = () => {
     return (
         <>
           <Navbar />
-          
+          <About />
         </>
     )
 }
