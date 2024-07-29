78f7e035d99978390a13328e5ecf5e76874c75c3
Patrick Gallardo <patrick@jbbgi.com>
2024-07-30 07:31:36 +0800

    Added blank space for testing

--- frontend/src/components/intro.tsx
+++ frontend/src/components/intro.tsx
@@ -6,7 +6,7 @@ const Intro = () => {
                 <div className="text-white text-[56px] font-bold arimo mb-[-1.5rem]">Hi, I'm</div>
                 <div className="text-[#ffb703] text-[56px] font-bold montserrat w-[365px] drop-shadow-[-2px_4px_#a85b03]">Juan Patrick Gallardo</div>
                 <div className="text-white text-[28px] font-bold arimo">Professional Software Developer</div>
-            </div>
+            </div> 
             <div className="sm:self-center sm:pt-0 pt-20">
                 <img className="rounded-full ml-28 w-[50%] sm:ml-0 sm:w-[100%]" src={require('../assets/front-photo.png')} alt="profile-photo"></img>
             </div>
