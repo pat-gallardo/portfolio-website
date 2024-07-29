37734206d14a9ac4e02f844e6cceebffa7e3b526
Patrick Gallardo <patrick@jbbgi.com>
2024-07-30 07:18:34 +0800

    Fixed intro-container padding

--- frontend/src/components/intro.tsx
+++ frontend/src/components/intro.tsx
@@ -2,7 +2,7 @@ const Intro = () => {
     return (
         <>
         <div className="flex flex-col-reverse sm:flex-row">
-            <div className="pl- pb-8 w-[100%] sm:pt-32 sm:pl-32 sm:w-[60%]">
+            <div className="pl-20 pb-8 w-[100%] sm:pt-32 sm:pl-32 sm:w-[60%]">
                 <div className="text-white text-[56px] font-bold arimo mb-[-1.5rem]">Hi, I'm</div>
                 <div className="text-[#ffb703] text-[56px] font-bold montserrat w-[365px] drop-shadow-[-2px_4px_#a85b03]">Juan Patrick Gallardo</div>
                 <div className="text-white text-[28px] font-bold arimo">Professional Software Developer</div>
