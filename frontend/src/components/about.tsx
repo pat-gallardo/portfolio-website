50975d5c327273c9f28f1c94da453b8148985132
Patrick Gallardo <juanpatrick.gallardo@gmail.com>
2024-07-31 14:30:59 +0800

    Added mobile responsiveness of about page

--- frontend/src/components/about.tsx
+++ frontend/src/components/about.tsx
@@ -4,12 +4,12 @@ const About = () => {
     return (
         <>
         <div className="flex mt-[5%] mb-[5%] flex-col sm:flex-row justify-between">
-            <div className="w-full sm:w-1/2  sm:self-center sm:pt-0 pt-20 ml-20 mr-20 sm:ml-5 sm:mr-5">
+            <div className="w-full sm:w-1/2  sm:self-center sm:pt-0 pt-[1rem] sm:pt-20 ml-5 pr-[2.5rem] sm:mr-5">
                 <img className="saturate-150 opacity-50 sm:ml-0 sm:w-full" src={require('../assets/front-photo2.png')} alt="profile-photo"></img>
             </div>
-            <div className="flex flex-col gap-[10%] w-full sm:w-1/2 ml-[10%]">
-                <div className="text-white text-[28px] montserrat ">{aboutMessage}</div>
-                <div className="text-white text-[28px] montserrat ">{aboutNextMessage}</div>
+            <div className="flex flex-col gap-[10%] w-full sm:w-1/2 m-3 p-3 sm:ml-[10%] text-wrap">
+                <div className="w-full text-white text-[28px] montserrat ">{aboutMessage}</div>
+                <div className="w-full text-white text-[28px] montserrat ">{aboutNextMessage}</div>
             </div> 
         </div>
         <div className="bg-white p-[0.5px] text-sm text-transparent">.</div>
