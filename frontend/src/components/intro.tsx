f4658bd67010abb2d5f963051563869798b5c2a5
Patrick Gallardo <juanpatrick.gallardo@gmail.com>
2024-08-02 09:13:29 +0800

    Removed text-generate-effect component.

--- frontend/src/components/intro.tsx
+++ frontend/src/components/intro.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { BackgroundGradient } from "./ui/background-icon";
-import { TextGenerateEffect } from "./ui/text-generate-effect";
  
 const Intro = () => {
     const message = 'Hi, I\'m'
@@ -13,7 +12,7 @@ const Intro = () => {
             <div className="pl-12 sm:pt-32 sm:pl-20 pt-2">
                 <div className="text-white text-[56px] font-bold arimo mb-[-1.5rem]">{message}</div>
                 <div className="w-[60%] sm:w-[100%] text-[#ffb703] text-[56px] font-bold montserrat w-[365px] drop-shadow-[-2px_4px_#a85b03]">{name}</div>
-                <TextGenerateEffect words={profession} duration={2} filter={false} />
+                <div className="text-white text-[28px] font-bold arimo">{profession}</div>
              </div> 
             <div className="sm:self-center sm:pt-0 pt-20 ml-20 mr-20 sm:ml-5 sm:mr-5">
             <BackgroundGradient className="rounded-full max-w-sm">
