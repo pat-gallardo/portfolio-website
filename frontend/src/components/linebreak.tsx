7842a07a4544df85ebac579ddd16a8610274c076
Patrick Gallardo <juanpatrick.gallardo@gmail.com>
2024-08-02 08:48:47 +0800

    Replaced assets and updated the styles of about page and linebreak

--- frontend/src/components/linebreak.tsx
+++ frontend/src/components/linebreak.tsx
@@ -6,7 +6,7 @@ interface LinebreakProps {
 const Linebreak: React.FC<LinebreakProps> = ({ setTitle }) => { 
     return (
         <>
-        <div className='flex flex-col justify-center pt-20'>
+        <div className='flex flex-col justify-center pt-20 relative z-10'>
             <div className="bg-[#fb8500] text-transparent">.</div>
             <div className="w-full absolute text-center text-white text-[60px] font-bold drop-shadow-[-2px_4px_#a85b03]">{setTitle}</div>
             <div className="bg-[#219ebc] p-3 text-transparent">.</div>
