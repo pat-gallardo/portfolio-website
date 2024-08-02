8aafa2a780e2a0a3aeb42d07aff282ef7c9467d4
Patrick Gallardo <juanpatrick.gallardo@gmail.com>
2024-08-02 16:27:02 +0800

    Added services containers, styles and images.

--- frontend/src/components/services.tsx
+++ frontend/src/components/services.tsx
@@ -1,10 +1,33 @@
+import { HoverEffect } from "./ui/card-hover-effect";
+import Services1 from "./ui/services-1"
+import Services2 from "./ui/services-2"
+import Services3 from "./ui/services-3"
+
 const Services = () => {
+    const servicesList = [
+        {
+        title: "AI Integration & Machine Learning",
+        description:
+          "Making AI's that can help your business strive",
+        image: <Services1 />
+      },
+      {
+        title: "Website & Application Development",
+        description:
+          "Making website that is user-friendly and croos platform",
+        image: <Services2 />
+        },
+      {
+        title: "Server & Database Management",
+        description:
+          "Having server or database issues? Let me know what it is",
+        image: <Services3 />
+      }]
+
     return (
         <>
-        <div className="flex flex-row space-x-1 w-full">
-            <div>a</div>
-            <div>b</div>
-            <div>c</div>
+         <div className="max-w-5xl mx-auto px-8">
+            <HoverEffect className="" items={servicesList} />
         </div>
         </>
     )
---
+++ frontend/src/components/ui/services-3.tsx
@@ -0,0 +1,9 @@
+const Services3 = () => {
+    return(
+        <>
+        <svg viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"><rect x="-2.4" y="-2.4" width="28.80" height="28.80" rx="14.4" fill="#219ebc" strokeWidth="0"></rect></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 18V6" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path> <path d="M20 6V18" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path> <path d="M12 10C16.4183 10 20 8.20914 20 6C20 3.79086 16.4183 2 12 2C7.58172 2 4 3.79086 4 6C4 8.20914 7.58172 10 12 10Z" stroke="#ffffff" strokeWidth="1.5"></path> <path d="M20 12C20 14.2091 16.4183 16 12 16C7.58172 16 4 14.2091 4 12" stroke="#ffffff" strokeWidth="1.5"></path> <path d="M20 18C20 20.2091 16.4183 22 12 22C7.58172 22 4 20.2091 4 18" stroke="#ffffff" strokeWidth="1.5"></path> </g></svg>
+        </>
+    )
+}
+
+export default Services3;
