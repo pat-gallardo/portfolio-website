9656b3a504d6b55e113ff36f1bbb5665b104ba23
Patrick <juanpatrick.gallardo@gmail.com>
2024-08-09 23:21:13 +0800

    Updated the design of the contacts page

--- frontend/src/components/Contacts.tsx
+++ frontend/src/components/Contacts.tsx
@@ -8,29 +8,29 @@ import {
   CardTitle,
 } from "./ui/card"
 import { Input } from "./ui/input"
-import { Label } from "./ui/label"
+import { Textarea } from "./ui/textarea"
 
 const Contacts = () => {
     return (<>
     <div id="contact" className="flex justify-center pt-20 pb-20">
-        <Card className="w-[75%] rounded bg-white">
+        <Card className="w-[75%] rounded bg-[#8ecae6] text-white">
       <CardHeader>
-        <CardTitle>Do you have questions or inquiries ?</CardTitle>
-        <CardDescription>Let me know by sending an email, I'll get in to you in a bit.</CardDescription>
+        <CardTitle className="text-[#023047]">Do you have questions or inquiries ?</CardTitle>
+        <CardDescription className="text-[#023047]">Let me know by sending an email, I'll get in to you in a bit.</CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" className="rounded" placeholder="Name of your project" />
+              <Input id="title" className="       text-[#023047]" placeholder="Subject" />
+              <Input id="email" className="       text-[#023047]" placeholder="Email" />
+              <Textarea id='content' className="text-[#023047] w-[100%] h-10" placeholder="Type your message here." />
             </div>
           </div>
         </form>
       </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline" className="rounded">Cancel</Button>
-        <Button variant="outline" className="rounded" >Deploy</Button>
+      <CardFooter className="flex justify-end">
+        <Button variant="outline" className="rounded text-[#023047] flex ">Send</Button>
       </CardFooter>
     </Card></div>
     </>)
